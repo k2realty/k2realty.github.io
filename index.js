682b9d08ca9c2970809c196f1fc0096a548a616f
Ethan Marchand <admin@k2r.ca>
2023-02-21 11:53:21 -0700

    Fixed Javascript

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const togglenav = () => {
-    const sidenav = document.querySelector("#navbar")
-    sidenav.classList.toggle('shownav');
+    document.querySelector("#navbar").classList.toggle('shownav');
+    let open = document.querySelector("#navbar").classList.contains('shownav');
+    if (open) {
+        document.querySelector("#menubtn").textContent = "close"
+    } else {
+        document.querySelector("#menubtn").textContent = "menu"
+    }
+
 }
