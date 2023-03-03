5326cd18c6be9b2934dfcb990b75383821b9189a
Ethan Marchand <admin@k2r.ca>
2023-03-03 14:42:55 -0700

    Partial email repair

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const togglenav = () => {
 
 }
 
-window.addEventListener("touchstart", function(e){
+window.addEventListener("touchstart", e => {
     if (!document.getElementById('navbar').contains(e.target) && (!document.getElementById('menubtn').contains(e.target))){
     document.getElementById('navbar').classList.remove("w-52");
     document.getElementById('navbar').classList.add("w-0");
@@ -26,24 +26,57 @@ window.addEventListener("touchstart", function(e){
   } 
 })
 
-const sendMail = () => {
-    let templateParams = {
-        fname : document.querySelector("#fname").value,
-        lname : document.querySelector("#lname").value,
-        email : document.querySelector("#email").value,
-        phone : document.querySelector("#phone").value,
-        contact_Method : document.querySelector("#contact_Method").value,
-        subject : document.querySelector("#subject").value,
-        street_Address : document.querySelector("#street_Address").value,
-        city : document.querySelector("#city").value,
-        province : document.querySelector("#province").value,
-        postal_Code : document.querySelector("#postal_Code").value
-    }
-        emailjs.send('service_0j957un', 'template_0qklrj3', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
     
-}
+    const fname = form.fname.value.trim();
+    const fnameErr = form.querySelector("#fnameErr");
+    const lname = form.lname.value.trim();
+    const email = form.email.value.trim();
+    const emailErr = form.querySelector("#emailErr");
+    const phone = form.phone.value.trim();
+    const contactMethod = form.contactMethod.value.trim();
+    const subject = form.subject.value.trim();
+    const address = form.address.value.trim();
+   
+    const err = {};
+    
+    if (fname.length === 0){
+        err.fname = "First name is missing";
+        fnameErr.innerText = err.fname
+        fnameErr.classList.remove("invisible");
+    } else {
+        delete err.fname;
+        fnameErr.classList.add("invisible");
+    }
+    if (email.length < 6){
+        err.email = "Valid email is missing";
+        emailErr.innerText = err.email;
+        emailErr.classList.remove("invisible");
+    } else {
+        delete err.email;
+        emailErr.classList.add("invisible");
+    }
+    if (!Object.keys(err).length > 0){
+        let templateParams = {
+            fname : fname,
+            lname : lname,
+            email : email,
+            phone : phone,
+            contactMethod : contactMethod,
+            subject : subject,
+            address : address,
+        }
+
+        emailjs.send('service_0j957un', 'template_0qklrj3', templateParams).then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        alert("Someone will be in touch as soon as possible.");
+        location.reload();
+        }, function(error) {
+        console.log('FAILED...', error);
+        });
+    } 
+})
